4715c8596545d980e99a3c5d1d43e329f0adcf1d
dev3 <dev3@dev2s-MacBook-5.local>
2017-08-02 10:50:22 -0300

    Updating host.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # react-native-app-emma
+
+### Install dependencies:
+
+```
+yarn install
+```
+
+### Run storybook:
+
+```
+yarn run storybook
+```
+
+### Start the simulator:
+
+```
+npm run ios
+```
--- storybook/index.js
+++ storybook/index.js
@@ -1,9 +1,17 @@
-import { getStorybookUI, configure } from '@storybook/react-native';
+import {
+  getStorybookUI,
+  configure
+} from '@storybook/react-native';
 
-// import stories
+// Import stories
 configure(() => {
   require('./stories');
 }, module);
 
-const StorybookUI = getStorybookUI({ port: 7007, host: '192.168.0.8' });
+const StorybookUI = getStorybookUI({
+  port: 7007,
+  // TODO: Make host parameterizable, change it in package.json as well.
+  host: '10.255.255.132'
+});
+
 export default StorybookUI;
